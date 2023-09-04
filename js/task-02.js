2a40574d9b40a6318774c1dae94b8eed0edb6e31
Piotrporadowski <122733517+Piotrporadowski@users.noreply.github.com>
2023-09-04 20:26:08 +0200

    e

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
+const ingredientsList = document.getElementById("ingredients");
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
 ];
+
+
+const fragment = document.createDocumentFragment();
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  fragment.appendChild(listItem);
+});
+
+
+ingredientsList.appendChild(fragment);
